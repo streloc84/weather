46d4ed940d0aa5dcc803f967707050dae51bcb28
weather11 <streloc84@mail.ru>
2020-07-07 22:19:42 +0400

    Updates

---
+++ precache-manifest.7a0cb84e7eab98a6478f48eaef4a2df2.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bb2925f59a6f8bb5b426cfe5c0fd2cf",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "06d048abc5510e516be5",
+    "url": "/weather/static/css/main.108fd8e0.chunk.css"
+  },
+  {
+    "revision": "8940f890adcf1b4aa4aa",
+    "url": "/weather/static/js/2.d591044a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather/static/js/2.d591044a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06d048abc5510e516be5",
+    "url": "/weather/static/js/main.00c81810.chunk.js"
+  },
+  {
+    "revision": "65a135568c88d392bf25",
+    "url": "/weather/static/js/runtime-main.d8725fa2.js"
+  },
+  {
+    "revision": "f54ac30dd647d9cc091f2da0898d7b62",
+    "url": "/weather/static/media/clouds.f54ac30d.gif"
+  },
+  {
+    "revision": "c3e68f186cc27c1696ee4dd14c581d80",
+    "url": "/weather/static/media/rain.c3e68f18.gif"
+  },
+  {
+    "revision": "f490706632dbe4a6385c4f7d549ae2d3",
+    "url": "/weather/static/media/snow.f4907066.gif"
+  },
+  {
+    "revision": "c4e6e582a6bf09c6e10514e46723712d",
+    "url": "/weather/static/media/sun.c4e6e582.gif"
+  },
+  {
+    "revision": "4a9b1c501d491411ab9a40b2626f1819",
+    "url": "/weather/static/media/thunder.4a9b1c50.gif"
+  }
+]);
